a472db31024ea9d05a80f2e4491ad8dd3d7ddbb7
Marina Oliveira <marina09oliveira@gmail.com>
2020-09-11 16:01:31 -0300

    Components, Props & PropTypes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React , { Component} from 'react';
+import Navbar from './components/layout/Navbar'
 import './App.css';
 
 class App extends Component{
   render(){
-    const name = 'John Doe';
-    const showName = true;
-
     return (
       <div className="App">
-       <h1>Hello {showName && name}</h1>
+      <Navbar />
       </div>
     );
   }
