cdda0374f419fa20d99d2d793ae4a1c5a86c14c1
Marina Oliveira <marina09oliveira@gmail.com>
2020-09-15 19:20:24 -0300

    fix repos function

--- src/App.js
+++ src/App.js
@@ -47,13 +47,14 @@ class App extends Component{
   }
 
   // Get user repos
-  getUserRepos = async (username) => {
+  getUserRepos = async username => {
     this.setState({ loading: true });
-
-    const res = await axios.get(`https://api.github.com/users/${username}/repo?per_page=5&s=creted:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
 
     this.setState({ repos: res.data, loading: false });
-  }
+  };
 
   // Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
