75a93a5022d43dea91be2532904718f97c3ca529
Marina Oliveira <marina09oliveira@gmail.com>
2020-09-17 18:35:18 -0300

    Moving User State To Context

--- src/components/Users/User.js
+++ src/components/Users/User.js
@@ -1,100 +1,122 @@
-import React, { Fragment, useEffect } from 'react';
-
+import React, { Fragment, useEffect, useContext } from 'react';
 import Spiner from '../layout/Spinner';
-import Repos from '../repos/Repos'
-
+import Repos from '../repos/Repos';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import GithubContext from '../../context/github/githubContext';
 
-const User = ({ user , loading, getUser, getUserRepos, repos, match}) => {
+const User = ({ getUserRepos, repos, match }) => {
+  const githubContext = useContext(GithubContext);
+
+  const { getUser, loading, user } = githubContext;
+
   useEffect(() => {
     getUser(match.params.login);
     getUserRepos(match.params.login);
-  }, [getUser, getUserRepos, match.params.login]);
+    // eslint-disable-next-line
+  }, []);
 
-    const {
-      name,
-      avatar_url,
-      location,
-      bio,
-      blog,
-      company,
-      login,
-      html_url,
-      followers,
-      following,
-      public_repos,
-      public_gists,
-      hireable
-    } = user;
+  const {
+    name,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    company,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+  } = user;
 
-    if(loading) return <Spiner />
+  if (loading) return <Spiner />;
 
-    return <Fragment>
-      <Link to='/' className='btn btn-light'>
+  return (
+    <Fragment>
+      <Link to="/" className="btn btn-light">
         Back To Search
       </Link>
-      Hireable: {' '}
-      {hireable ? (<i className="fas fa-check text-success" /> )
-      : (<i className="fas fa-times-circle text-danger" />)}
+      Hireable:{' '}
+      {hireable ? (
+        <i className="fas fa-check text-success" />
+      ) : (
+        <i className="fas fa-times-circle text-danger" />
+      )}
       <div className="card grid-2">
         <div className="all-center">
-          <img 
-            src={avatar_url} 
-            className="round-img" 
-            alt="avatar" 
-            style={{width: '150px'}}
+          <img
+            src={avatar_url}
+            className="round-img"
+            alt="avatar"
+            style={{ width: '150px' }}
           />
           <h1>{name}</h1>
           <p>Location: {location}</p>
         </div>
         <div>
-          {bio && 
+          {bio && (
             <Fragment>
-            <h3>Bio</h3>
-            <p>{bio}</p>
-            </Fragment>}
+              <h3>Bio</h3>
+              <p>{bio}</p>
+            </Fragment>
+          )}
           <a href={html_url} className="btn btn-dark my-1">
             Visit Github Profile
           </a>
           <ul>
             <li>
-              {login && <Fragment>
-                <strong>Username: </strong> {login}
-                </Fragment>}
+              {login && (
+                <Fragment>
+                  <strong>Username: </strong> {login}
+                </Fragment>
+              )}
             </li>
             <li>
-              {company && <Fragment>
-                <strong>Company: </strong> {company}
-                </Fragment>}
+              {company && (
+                <Fragment>
+                  <strong>Company: </strong> {company}
+                </Fragment>
+              )}
             </li>
             <li>
-              {blog && <Fragment>
-                <strong>Website: </strong> {blog}
-                </Fragment>}
+              {blog && (
+                <Fragment>
+                  <strong>Website: </strong> {blog}
+                </Fragment>
+              )}
             </li>
           </ul>
         </div>
       </div>
-
       <div className="card text-center">
         <div className="badge badge-primary">Followers: {followers}</div>
         <div className="badge badge-success">Following: {following}</div>
         <div className="badge badge-danger">Public Repos: {public_repos}</div>
         <div className="badge badge-dark">Public Gists: {public_gists}</div>
       </div>
-
       <Repos repos={repos} />
-    </Fragment>;
-
-}
+    </Fragment>
+  );
+};
 
 User.propTypes = {
-  loading: PropTypes.bool,
-  user: PropTypes.object.isRequired,
   repos: PropTypes.array.isRequired,
-  getUser: PropTypes.func.isRequired,
-  getUserRepos: PropTypes.func.isRequired
-}
+  getUserRepos: PropTypes.func.isRequired,
+};
 
 export default User;
+
+/* Maximum update depth exceeded. This can happen when a component calls setState inside useEffect, but useEffect either doesn't have a dependency array, or one of the dependencies changes on every render.
+    in User (at App.js:63)
+    in Route (at App.js:59)
+    in Switch (at App.js:47)
+    in div (at App.js:45)
+    in div (at App.js:43)
+    in Router (created by BrowserRouter)
+    in BrowserRouter (at App.js:42)
+    in GithubState (at App.js:41)
+    in App (at src/index.js:9)
+    in StrictMode (at src/index.js:8) */
