42d824eeff2ce56e5850d7ee636056583a2b5e07
Marina Oliveira <marina09oliveira@gmail.com>
2020-09-17 16:28:09 -0300

    Create Reducer & Actions

--- src/components/Users/Search.js
+++ src/components/Users/Search.js
@@ -1,41 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import PropTypes from 'prop-types';
+import GithubContex from '../../context/github/githubContext';
 
-const Search = ({searchUsers, showClear, clearUsers, setAlert}) => {
+const Search = ({ showClear, clearUsers, setAlert }) => {
+  const githubContext = useContext(GithubContex);
+
   const [text, setText] = useState('');
 
-  const onSubmit = e => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    if(text === ''){
+    if (text === '') {
       setAlert('Please enter something', 'danger-color');
-    }else{
-      searchUsers(text);
+    } else {
+      githubContext.searchUsers(text);
       setText('');
     }
-  }
+  };
 
-  const onChange = e => setText(e.target.value);
+  const onChange = (e) => setText(e.target.value);
 
   return (
-      <div className="form">
-        <form onSubmit={onSubmit} className="form">
-          <input 
-            type="text"
-            name="text" 
-            placeholder="Search users..."
-            value={text}
-            onChange={onChange}
-          />
-          <input type="submit" value="Search"className="btn btn-dark btn-block"/>
-        </form>
-        {showClear && <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>}  
-      </div>
-    )
-
-}
+    <div className="form">
+      <form onSubmit={onSubmit} className="form">
+        <input
+          type="text"
+          name="text"
+          placeholder="Search users..."
+          value={text}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
+        />
+      </form>
+      {showClear && (
+        <button className="btn btn-light btn-block" onClick={clearUsers}>
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
 
 Search.propTypes = {
-  searchUsers: PropTypes.func.isRequired,
   clearUsers: PropTypes.func.isRequired,
   showClear: PropTypes.bool.isRequired,
   setAlert: PropTypes.func.isRequired,
--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -21,10 +21,25 @@ const GithubState = (props) => {
   const [state, dispatch] = useReducer(GithubReducer, initalState);
 
   //Search Users
+  // Search github users
+  const searchUsers = async (text) => {
+    setLoading();
+
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({
+      type: SEARCH_USERS,
+      payload: res.data.items,
+    });
+  };
+
   //Get User
   // Get Repos
   //Clear Users
   //Set Loading
+  const setLoading = () => dispatch({ type: SET_LOADING });
 
   return (
     <GithubContext.Provider
@@ -33,6 +48,7 @@ const GithubState = (props) => {
         user: state.user,
         repos: state.repos,
         loading: state.loading,
+        searchUsers,
       }}
     >
       {props.children}
